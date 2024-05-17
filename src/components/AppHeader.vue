9a5532b898309c6f4fec8140c05679ff530d809c
Rekash <benmehal.joris@gmail.com>
2024-05-17 17:04:50 +0200

    🎨 Update social media links in AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,10 +5,10 @@
     <header class="bg-gray-800 text-white p-4 flex align-middle justify-between">
         <h1 class="text-2xl font-semibold">Expense Tracker</h1>
         <nav class="flex justify-end gap-4">
-            <a href="">
+            <a href="https://github.com/notREKASH" target="_blank">
                 <img src="../assets/github.svg" alt="GitHub" class="w-6 h-6" />
             </a>
-            <a href="">
+            <a href="https://www.linkedin.com/in/joris-benmehal-6266a427a/" target="_blank">
                 <img src="../assets/linkedin.svg" alt="LinkedIn" class="w-6 h-6" />
             </a>
         </nav>
